7cfe30da8dcc7a5d832c9a05b2d2f9b59df982b6
José Beltre <552006jose@gmail.com>
2025-05-12 19:12:53 -0400

    style(CartonItemDetails): update styles for different themes

--- frontend/src/components/CartonItemDetails.jsx
+++ frontend/src/components/CartonItemDetails.jsx
@@ -1,7 +1,7 @@
 export function CartonItemDetails ({ name, amount, openModal }) {
   return (
-    <div onClick={openModal} className='flex justify-between dark:bg-[#1f1f1f] rounded-md py-1 p-4 cursor-pointer hover:bg-white/10 transition-colors'>
-      <p className='text-white'>{name}</p>
+    <div onClick={openModal} className='flex justify-between bg-white/75 dark:bg-[#1f1f1f] rounded-md py-1 p-4 cursor-pointer hover:bg-white/10 transition-colors'>
+      <p className='dark:text-white'>{name}</p>
       <p>${amount}</p>
     </div>
   )
