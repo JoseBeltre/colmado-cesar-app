8c8d05764efc8e9da4a3f6e19a99f151ac90a790
Jose Beltre <552006jose@gmail.com>
2025-05-18 21:43:21 -0400

    fix(Button): add leading-3 class to button for improved text alignment

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button ({ children, className = '', ...rest }) {
   return (
-    <button className={`flex gap-1 justify-center items-center flex-1 bg-primary p-2 px-3 rounded-md font-medium text-sm cursor-pointer ${className}`} {...rest}>
+    <button className={`flex gap-1 justify-center items-center flex-1 bg-primary p-2 px-3 rounded-md font-medium text-sm cursor-pointer leading-3 ${className}`} {...rest}>
       {children}
     </button>
   )
