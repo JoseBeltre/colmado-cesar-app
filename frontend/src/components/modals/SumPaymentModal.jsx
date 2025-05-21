c645685620a22bbf0c0076290fddc65603750c77
José Beltre <552006jose@gmail.com>
2025-05-21 19:28:38 -0400

    fix(SumPaymentModal): format debt message and add description input field

--- frontend/src/components/modals/SumPaymentModal.jsx
+++ frontend/src/components/modals/SumPaymentModal.jsx
@@ -2,20 +2,22 @@ import { BackgroundModal } from './BackgroundModal'
 import { ConfirmModal } from './ConfirmModal'
 import { Input } from '../Input'
 import { useState } from 'react'
+import { formatCurrency } from '../../utils/utils'
 
 export function SumPaymentModal ({ closeModal, client }) {
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
   const fullname = client.firstName.split(' ')[0] + ' ' + client.lastName.split(' ')[0]
   const title = <>Sumar pago a <span>{fullname}</span></>
   const [amount, setAmount] = useState(null)
+  const [description, setDescription] = useState('Pago de deuda')
 
   const debt = client.balance + parseInt(amount)
-  console.log(debt)
+  const formattedDebt = formatCurrency(debt)
   let personalizedMsg
   if (debt < 0) {
-    personalizedMsg = `Esta acción rebajará su deuda a $${debt}.`
+    personalizedMsg = `Esta acción rebajará su deuda a ${formattedDebt}.`
   } else if (debt > 0) {
-    personalizedMsg = (client.balance === 0) ? 'Este monto se añadirá a su cuenta.' : `Esta acción saldará su deuda y el colmado le deberá ${debt}.`
+    personalizedMsg = (client.balance === 0) ? 'Este monto se añadirá a su cuenta.' : `Esta acción saldará su deuda y el colmado le deberá ${formattedDebt}.`
   } else {
     personalizedMsg = 'Esta acción saldará su deuda.'
   }
@@ -26,16 +28,27 @@ export function SumPaymentModal ({ closeModal, client }) {
         title={title}
         onConfirm={() => setIsConfirmModalOpen(!isConfirmModalOpen)}
       >
-        <Input
-          name='amount'
-          title='Ingresar monto a sumar'
-          placeholder='ej. 400'
-          value={amount}
-          type='number'
-          min={1}
-          max={5000}
-          onChange={e => setAmount(e.target.value)}
-        />
+        <div className='grid gap-3'>
+          <Input
+            name='amount'
+            title='Ingresar monto a sumar'
+            placeholder='ej. 400'
+            value={amount}
+            type='number'
+            min={1}
+            max={5000}
+            onChange={e => setAmount(e.target.value)}
+          />
+          <Input
+            name='description'
+            title='Concepto'
+            placeholder='ej. Pago de deuda.'
+            value={description}
+            min={1}
+            max={5000}
+            onChange={e => setDescription(e.target.value)}
+          />
+        </div>
       </ConfirmModal>
       {
         isConfirmModalOpen &&
