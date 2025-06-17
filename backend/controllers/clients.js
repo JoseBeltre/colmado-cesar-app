368f9b1bf3b165702ceb73963fb39a5010cda3bd
José Beltre <552006jose@gmail.com>
2025-06-17 15:34:56 -0400

    feat(clients): add transaction handling in ClientsController and ClientsModel

--- backend/controllers/clients.js
+++ backend/controllers/clients.js
@@ -1,6 +1,7 @@
 import { UserModel } from '../models/auth.js'
 import { ClientsModel } from '../models/clients.js'
 import { validateClient, validatePartialClient } from '../schemas/client.js'
+import { validateTransaction } from '../schemas/transaction.js'
 import { BadRequestError, errorHandler } from '../utils/errors.js'
 
 export class ClientsController {
@@ -64,12 +65,15 @@ export class ClientsController {
   static async update (req, res) {
     const { id } = req.params
     try {
+      if (isNaN(Number(id))) {
+        throw new BadRequestError('ID debe ser un número.')
+      }
+      await ClientsModel.getOne({ id })
       const result = validatePartialClient(req.body)
       if (!result.success) {
         throw new BadRequestError(result.error.message)
       }
 
-      await ClientsModel.getOne({ id })
       const client = {
         id: parseInt(id),
         ...result.data
@@ -85,13 +89,41 @@ export class ClientsController {
 
   static async delete (req, res) {
     const { id } = req.params
-    if (!id) throw new BadRequestError('No se proveyó el ID de usuario.')
     try {
+      if (isNaN(Number(id))) {
+        throw new BadRequestError('ID debe ser un número.')
+      }
       const wasDeleted = await ClientsModel.delete({ id })
       if (!wasDeleted) {
         throw new Error('Error al eliminar el cliente.')
       }
       return res.status(200).json({ message: 'cliente eliminado correctamente.' })
+    } catch (error) {
+      errorHandler(res, error)
+    }
+  }
+
+  static async addTransaction (req, res) {
+    const { id } = req.params
+    try {
+      const clientId = Number(id)
+      if (isNaN(clientId)) {
+        throw new BadRequestError('ID debe ser número.')
+      }
+      await ClientsModel.getOne({ id })
+
+      const result = await validateTransaction(req.body)
+      if (!result.success) {
+        throw new BadRequestError(result.error.message)
+      }
+
+      const transaction = {
+        clientId: parseInt(id),
+        ...result.data
+      }
+
+      const newTransaction = await ClientsModel.addTransaction({ transaction })
+      return res.status(200).json(newTransaction)
     } catch (error) {
       errorHandler(res, error)
     }
--- backend/models/clients.js
+++ backend/models/clients.js
@@ -92,7 +92,7 @@ export class ClientsModel {
     if (!result.affectedRows) {
       throw new Error('No se pudo actualizar la información del cliente.')
     }
-    const updatedClient = this.getOne({ id: client.id })
+    const updatedClient = await this.getOne({ id: client.id })
 
     return updatedClient
   }
@@ -104,5 +104,36 @@ export class ClientsModel {
       throw new Error('Error al eliminar el cliente.')
     }
     return true
+  }
+
+  static async getOneTransaction ({ id }) {
+    const [transaction] = await conn.query(`
+      SELECT 
+      id,
+      amount,
+      description,
+      client_id AS clientId,
+      BIN_TO_UUID(employee_id) as employeeId
+      FROM clients_transactions
+      WHERE id = ? `, id)
+
+    if (transaction.length === 0) {
+      throw new NotFoundError('Esta transacción no existe.')
+    }
+    return transaction[0]
+  }
+
+  static async addTransaction ({ transaction }) {
+    const { amount, description, clientId, employeeId } = transaction
+    const [result] = await conn.query(`
+      INSERT INTO clients_transactions (amount, description, client_id, employee_id)
+      VALUES (?, ?, ?, UUID_TO_BIN(?))
+      `, [amount, description, clientId, employeeId])
+    if (!result.insertId) {
+      throw new Error('Error al agregar la transación.')
+    }
+
+    const newTransaction = await this.getOneTransaction({ id: result.insertId })
+    return newTransaction
   }
 }
