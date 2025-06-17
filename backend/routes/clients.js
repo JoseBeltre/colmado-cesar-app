362123cc766230b551f05eb26babd39377847192
José Beltre <552006jose@gmail.com>
2025-06-17 15:34:38 -0400

    refactor(clients): reorder route definitions for better organization and add post client transaction route

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -3,8 +3,13 @@ import { ClientsController } from '../controllers/clients.js'
 
 export const clientsRouter = Router()
 
+// GET
 clientsRouter.get('/', ClientsController.getAll)
 clientsRouter.get('/:id', ClientsController.getOne)
-clientsRouter.delete('/:id', ClientsController.delete)
+// POST
 clientsRouter.post('/', ClientsController.create)
+clientsRouter.post('/:id/transaction', ClientsController.addTransaction)
+// DELETE
+clientsRouter.delete('/:id', ClientsController.delete)
+// PATCH
 clientsRouter.patch('/:id', ClientsController.update)
